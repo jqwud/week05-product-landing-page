6f72fcace11a5bd46dfa5f275f85d37961f7b4a1
Joseph Dion Caday <josephdioncaday@gmail.com>
2026-09-10 22:59:46 +0800

    refactor: reorganize views into layouts and pages folders

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <x-navbar />
+    <x-hero />
+    <x-features-section />
+    <x-pricing-section />
+    <x-testimonials-section />
+    <x-pharmacy-highlight />
+    <x-cta-section />
+    <x-contact-section />
+    <x-footer />
+
+@endsection
